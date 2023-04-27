dda3a9e782bd8fa0faa071ce97b8c189cca78fb8
Yakumaa <maharjanshrish8@gmail.com>
2023-04-27 19:09:32 +0545

    Updating cart item

--- store/views.py
+++ store/views.py
@@ -10,7 +10,7 @@ from rest_framework import status
 from store.pagination import DefaultPagination
 from .filters import ProductFilter
 from .models import Cart, CartItem, Product, Collection, Review
-from .serializers import AddCartItemSerializer, CartItemSerializer, CartSerializer, ProductSerializer, CollectionSerializer, ReviewSerializer
+from .serializers import AddCartItemSerializer, CartItemSerializer, CartSerializer, ProductSerializer, CollectionSerializer, ReviewSerializer, UpdateCartItemSerializer
 
 # Create your views here.
 
@@ -65,10 +65,13 @@ class CartViewSet(CreateModelMixin, RetrieveModelMixin, DestroyModelMixin, Gener
 
 
 class CartItemViewSet(ModelViewSet):
+    http_method_names = ['get', 'post', 'patch', 'delete']
 
     def get_serializer_class(self):
         if self.request.method == 'POST':
             return AddCartItemSerializer
+        elif self.request.method == 'PATCH':
+            return UpdateCartItemSerializer
         return CartItemSerializer
 
     def get_serializer_context(self):
