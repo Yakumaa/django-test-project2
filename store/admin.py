163c1e56d3ce08c32886d7a8775d290cc91aa716
Yakumaa <maharjanshrish8@gmail.com>
2023-04-29 19:05:31 +0545

    created user profiles

--- store/admin.py
+++ store/admin.py
@@ -86,9 +86,10 @@ class CollectionAdmin(admin.ModelAdmin):
 class CustomerAdmin(admin.ModelAdmin):
     list_display = ['first_name', 'last_name',
                     'membership', 'orders']
-    search_fields = ['customer']
     list_editable = ['membership']
-    ordering = ['first_name', 'last_name']
+    list_per_page = 10
+    list_select_related = ['user']
+    ordering = ['user__first_name', 'user__last_name']
     search_fields = ['first_name__istartswith', 'last_name__istartswith']
 
     @admin.display(ordering='orders_count')
