1a750e570c1c8aa63b311d611a61d365221657eb
Yakumaa <maharjanshrish8@gmail.com>
2023-05-10 22:38:55 +0545

    updated order

--- store/views.py
+++ store/views.py
@@ -30,6 +30,7 @@ from .serializers import (
     CollectionSerializer,
     ReviewSerializer,
     UpdateCartItemSerializer,
+    UpdateOrderSerializer,
 )
 
 # Create your views here.
@@ -158,6 +159,8 @@ class OrderViewSet(ModelViewSet):
     def get_serializer_class(self):
         if self.request.method == "POST":
             return CreateOrderSerializer
+        elif self.request.method == "PATCH":
+            return UpdateOrderSerializer
         return OrderSerializer
 
     def get_queryset(self):
