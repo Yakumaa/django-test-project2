4e0fa68fa3ce2063a4c9831a269af0a1a15c6eaf
Yakumaa <maharjanshrish8@gmail.com>
2023-05-12 23:23:46 +0545

    sent emails

--- playground/views.py
+++ playground/views.py
@@ -1,15 +1,10 @@
+from django.core.mail import send_mail, mail_admins, BadHeaderError
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.core.exceptions import ObjectDoesNotExist
-from django.db.models import Q, F, Value, Count, ExpressionWrapper, DecimalField
-from django.db.models.functions import Concat
-from django.db import transaction, connection
-from django.contrib.contenttypes.models import ContentType
-from store.models import Product, OrderItem, Customer, Collection, Order
-from tags.models import TaggedItem
 
 
 def say_hello(request):
-    queryset = Product.objects.raw('SELECT * FROM store_product')
-
-    return render(request, 'hello.html', {'name': 'Mosh', 'result': list(queryset)})
+    try:
+        mail_admins("subject", "message", html_message="message")
+    except BadHeaderError:
+        pass
+    return render(request, "hello.html", {"name": "Mosh"})
