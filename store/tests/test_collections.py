29f355e75888f5980ed871d67bdb10e957ec996f
Yakumaa <maharjanshrish8@gmail.com>
2023-05-29 21:52:59 +0545

    authenticating user

--- store/tests/test_collections.py
+++ store/tests/test_collections.py
@@ -4,7 +4,6 @@ import pytest
 
 @pytest.mark.django_db
 class TestCreateCollection:
-
     def test_if_user_is_anonymous_returns_401(self):
         #arrange
 
@@ -14,3 +13,14 @@ class TestCreateCollection:
 
         #assert
         assert response.status_code == status.HTTP_401_UNAUTHORIZED
+
+    def test_if_user_is_not_admin_returns_403(self):
+        #arrange
+
+        #act
+        client = APIClient()
+        client.force_authenticate(user={})
+        response = client.post('/store/collections/', {'title': 'a'})
+
+        #assert
+        assert response.status_code == status.HTTP_403_FORBIDDEN
